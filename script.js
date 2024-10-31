8f971a953e23e03a4e1f6d0c811efdd3f44946d4
Ramkumar1216 <Ramkumar121600@gmail.com>
2024-10-31 11:49:22 +0530

    Ram

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+    const menu =document.querySelector(".menu-links");
+    const links =document.querySelector(".hamburgar-icon");
+    menu.classList.toggle("open");
+    menu.classList.toggle("open");
+}
